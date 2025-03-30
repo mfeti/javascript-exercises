f13b79f02a468d3eef7661938950c1b63be1cfa4
mfeti <mfeti2175@gmail.com>
2025-03-30 08:53:59 +0300

    Update sumAll.js and sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (initialNum, endNum) {
+  if (!Number.isInteger(initialNum) || !Number.isInteger(endNum))
+    return "ERROR";
+  if (initialNum < 0 || endNum < 0) return "ERROR";
+  let sum = 0;
+  if (initialNum > endNum) {
+    let temp = initialNum;
+    initialNum = endNum;
+    endNum = temp;
+  }
+  for (let i = initialNum; i <= endNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
