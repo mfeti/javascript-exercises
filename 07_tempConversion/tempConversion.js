ca7f36dcf47a2ed4a2a09e48fbd5f0f913a1ed40
mfeti <mfeti2175@gmail.com>
2025-03-30 09:24:06 +0300

    Update tempConversion.js and tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let convertedValue = ((fahrenheit - 32) * 5) / 9;
+  return Number.isInteger(convertedValue)
+    ? convertedValue
+    : +convertedValue.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let convertedValue = (celsius * 9) / 5 + 32;
+  return Number.isInteger(convertedValue)
+    ? convertedValue
+    : +convertedValue.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
