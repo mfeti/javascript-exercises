0cf46966f0a9d58c9a0b7ee5207cba9714e2c73a
mfeti <mfeti2175@gmail.com>
2025-03-30 09:45:26 +0300

    Update calculator.js and calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (...args) {
+  return args.reduce((sum, cur) => sum + cur, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  return args.slice(1).reduce((result, cur) => result - cur, args[0]);
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, cur) => sum + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((result, cur) => result * cur, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 1 || num === 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +30,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
