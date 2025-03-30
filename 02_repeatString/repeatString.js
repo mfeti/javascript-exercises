9c3b11591054a2e769d40f14ac0202386039cf86
mfeti <mfeti2175@gmail.com>
2025-03-30 07:42:51 +0300

    Fix issues related to expected result in repeatingString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let text = "";
+  for (let i = 0; i < num; i++) {
+    text += str;
+  }
+  return text;
+  //return str.repeat(num);
 };
 
 // Do not edit below this line
